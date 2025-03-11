cef1b4f9f36b09584c3d9600210b806f602f1aa7
Ampte B Marak <amptebmarak@gmail.com>
2025-03-12 00:24:21 +0530

    Remove icons from introduction file

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,44 +1,10 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
-const Introduction = ({userName}) => {
+const Introduction = () => {
 
-    const navigate = useNavigate();
-
-    const goToLoginPage = () => {
-        navigate('/login');
-    };
     return(
         <>
-        <div className='introduction'>
-           <div className='icons'>
-           <div>
-            <button className='comment'
-            style={{
-                padding: '10px'
-            }}
-            >
-            <i class="fa-regular fa-comment"
-            style={{
-                fontSize: 'large'
-            }}
-            ></i>
-            </button>
-            </div>
-            <div>{userName}
-               <button className='user-circle' onClick={goToLoginPage}
-               style={{
-                padding: '10px'
-               }}
-               >
-               <i class="fa-regular fa-circle-user"
-               style={{
-                fontSize: 'large'
-               }}></i>
-               </button>
-            </div>
-           </div>
-        </div>
         </>
     );
 };
