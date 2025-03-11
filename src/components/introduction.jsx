535116010ebda6f7b6fb865d02a7bcc26d40dda1
Ampte B Marak <amptebmarak@gmail.com>
2025-03-12 00:53:04 +0530

    Add small changes

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
 
-const Introduction = ({userName}) => {
+const Introduction = () => {
 
     return(
         <>
